f4e79bb2df5b0b22d76021cf9abf398d1a3a0127
Franck Bilong <mala.molotov24@gmail.com>
2021-04-25 05:34:59 +0200

    ma mise a jour2

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this._films = []
+  }
+
+    _loadFilms() {
+        getFilmsFromApiWithSearchedText("star").then(data => {
+          this._films = data.results
+          this.forceUpdate()
+        });
+    }
+
     render() {
       return (
-         <View>
-           <TextInput placeholder='Titre du film'/>
-           <Button title='Rechercher' onPress={() => {}}/>
-         </View>
+        <View style={styles.main_container}>
+           <TextInput style={styles.textinput} placeholder='Titre du film'/>
+           <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+           <FlatList
+             data={this._films}
+             keyExtractor={(item) => item.id.toString()}
+             renderItem={({item}) => <FilmItem film={item}/>}
+        />
+        </View>
       )
     }
 }
+
+const styles = StyleSheet.create({
+ main_container: {
+   flex: 1,
+   marginTop: 20
+ },
+  textinput: {
+   marginLeft: 5,
+   marginRight: 5,
+   height: 50,
+   borderColor: '#000000',
+   borderWidth: 1,
+   paddingLeft: 5
+ }
+})
 
 export default Search
