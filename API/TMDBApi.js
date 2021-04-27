9894a18ed0b4f0bd97162a4a70c3fcc4b965be52
Franck Bilong <mala.molotov24@gmail.com>
2021-04-27 10:14:32 +0200

    ma sauvegarde

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,9 +1,13 @@
 
 const API_TOKEN = "ad1b2d72001b2e950bbaef61ec29ab6e";
 
-export function getFilmsFromApiWithSearchedText(text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText(text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
+}
+
+export function getImageFromApi(name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
 }
